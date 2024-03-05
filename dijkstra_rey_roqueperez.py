35d030813838113ec6f6baf3fb4d27bf9a2569d3
Rey Roque <reyroque@umd.edu>
2024-03-04 22:31:28 -0500

    Added a function to represent the hexagon obstacle

--- dijkstra_rey_roqueperez.py
+++ dijkstra_rey_roqueperez.py
@@ -3,6 +3,7 @@
 # UID: 120498626
 
 import heapq as hq
+import time
 
 import numpy as np
 import matplotlib.pyplot as plt
@@ -60,10 +61,47 @@ def in_rectangle(object_coordinate, obstruction, padding):
 
     return return_bool
 
-def in_hexagon(obstacle):
-    return_bool = False
+# Will cause an error if x2 - x1 = 0
+def get_slope(coordinate_1, coordinate_2):
+    x1, y1 = coordinate_1
+    x2, y2 = coordinate_2
+    slope = (y2-y1)/(x2-x1)
+    return slope
 
-    return return_bool    
+# Returns f(x,y) for a half plane created by two points
+def half_plane(coordinate_1, coordinate_2,object_coordinate):
+    slope = get_slope(coordinate_1, coordinate_2)
+    x1, y1 = coordinate_1
+    x, y = object_coordinate
+    equation = slope*(x-x1)-y+y1
+    return equation
+
+def in_hexagon(object_coordinate, padding):
+    hexagon = get_hexagon_coordinates((650,250),150,90,padding)
+    H1 = False
+    H2 = False
+    H3 = False
+    H4 = False
+    H5 = False
+    H6 = False
+
+    # Variables H are booleans for the half-planes
+    if half_plane(hexagon[0],hexagon[1],object_coordinate) >= 0:
+        H1 = True
+    # H2 half plane is vertical
+    if object_coordinate[0] >= hexagon[1][0]:
+        H2 = True
+    if half_plane(hexagon[2],hexagon[3],object_coordinate) <= 0:
+        H3 = True
+    if half_plane(hexagon[3],hexagon[4],object_coordinate) <= 0:
+        H4 = True
+    # H5 half plane is vertical    
+    if object_coordinate[0] <= hexagon[5][0]:
+        H5 = True
+    if half_plane(hexagon[5],hexagon[0],object_coordinate) >= 0:
+        H6 = True
+
+    return (H1 & H2 & H3 & H4 & H5 & H6)    
 
 def in_obstacle(obstacles):
     return_bool = False
@@ -71,18 +109,18 @@ def in_obstacle(obstacles):
         if len(obstacle) == 2:
             if in_rectangle(start_coordinate, obstacle, 0):
                 return_bool = True
-        #elif len(obstacle) == 6:
-            #if in_hexagon(start_coordinate, obstacle, 0):
-                #return_bool = True
+        elif len(obstacle) == 6:
+            if in_hexagon(start_coordinate, obstacle, 5):
+                return_bool = True
     return return_bool
 
-def get_polygon_points(center_coordinate, radius, angle):
+def get_hexagon_coordinates(center_coordinate, radius, angle, padding):
     # Define angles for each point of the hexagon
     angles = np.deg2rad(np.arange(0, 360, 60) + angle)
     
     # Calculate x and y coordinates for each angle
-    x_coordinates = np.round(radius * np.cos(angles) + center_coordinate[0])
-    y_coordinates = np.round(radius * np.sin(angles) + center_coordinate[1])
+    x_coordinates = np.round((radius + padding) * np.cos(angles) + center_coordinate[0])
+    y_coordinates = np.round((radius + padding) * np.sin(angles) + center_coordinate[1])
     
     # Combine x and y coordinates into tuples
     coordinates = [(int(x), int(y)) for x, y in zip(x_coordinates, y_coordinates)]
@@ -95,9 +133,9 @@ def draw_obstacles(canvas, obstacles):
             start_y = 500 - obstacle[0][1]  # Invert y-value
             end_x = obstacle[1][0]
             end_y = 500 - obstacle[1][1]  # Invert y-value
-            start_point = (start_x, start_y)
-            end_point = (end_x, end_y)
-            cv2.rectangle(canvas, pt1=start_point, pt2=end_point, color=(0, 0, 0), thickness=-1)
+            start_coordinate = (start_x, start_y)
+            end_coordinate = (end_x, end_y)
+            cv2.rectangle(canvas, pt1=start_coordinate, pt2=end_coordinate, color=(0, 0, 0), thickness=-1)
         elif len(obstacle) == 6:
             polygon = np.array(obstacle)
             polygon = polygon.reshape(-1, 1, 2)
@@ -112,7 +150,7 @@ def obstacle_space():
     obstacle_list.append(((900,375),(1100,450)))
     obstacle_list.append(((1020,125),(1100,375)))
     
-    polygon = (get_polygon_points((650,250),150,90))
+    polygon = (get_hexagon_coordinates((650,250),150,90,0))
     obstacle_list.append(polygon)
 
     return obstacle_list
@@ -123,7 +161,7 @@ def dijkstra_algorithm():
 
 # Cost to come, index, parent node index, and coordinate values (x,y)
 goal_coordinate = (1200,250)
-start_coordinate = (0,250)
+start_coordinate = (450,250)
 open_queue = [(0, 0, 0, start_coordinate)]
 hq.heapify(open_queue)
 
@@ -146,3 +184,5 @@ draw_obstacles(canvas, obstacles)
 # Display canvas
 plt.imshow(canvas)
 plt.show()
+
+print(in_hexagon(start_coordinate,5))
