8b2a818c1ab87e8e75a967c7af9b18494863e08f
Rey Roque <reyroque@umd.edu>
2024-03-09 20:22:30 -0500

    Added Dijkstra and Backtracking, Added path visualization

--- dijkstra_rey_roqueperez.py
+++ dijkstra_rey_roqueperez.py
@@ -3,56 +3,61 @@
 # UID: 120498626
 
 import heapq as hq
+from queue import PriorityQueue
 import time
 
 import numpy as np
 import matplotlib.pyplot as plt
 import cv2
 
-def move_right(coordinate):
-    new_coordinate = [coordinate[0]+1, coordinate[1]]
-    return new_coordinate
+def move_right(node):
+    new_node = (node[0]+1, node[1])
+    return new_node
 
-def move_left(coordinate):
-    new_coordinate = [coordinate[0]-1, coordinate[1]]
-    return new_coordinate
+def move_left(node):
+    new_node = (node[0]-1, node[1])
+    return new_node
 
-def move_up(coordinate):
-    new_coordinate = [coordinate[0], coordinate[1]+1]
-    return new_coordinate
+def move_up(node):
+    new_node = (node[0], node[1]+1)
+    return new_node
 
-def move_down(coordinate):
-    new_coordinate = [coordinate[0], coordinate[1]-1]
-    return new_coordinate
+def move_down(node):
+    new_node = (node[0], node[1]-1)
+    return new_node
 
-def move_up_right(coordinate):
-    new_coordinate = [coordinate[0]+1, coordinate[1]+1]
-    return new_coordinate
+def move_up_right(node):
+    new_node = (node[0]+1, node[1]+1)
+    return new_node
 
-def move_up_left(coordinate):
-    new_coordinate = [coordinate[0]-1, coordinate[1]+1]
-    return new_coordinate
+def move_up_left(node):
+    new_node = (node[0]-1, node[1]+1)
+    return new_node
 
-def move_down_right(coordinate):
-    new_coordinate = [coordinate[0]+1, coordinate[1]-1]
-    return new_coordinate
+def move_down_right(node):
+    new_node = (node[0]+1, node[1]-1)
+    return new_node
 
-def move_down_left(coordinate):
-    new_coordinate = [coordinate[0]-1, coordinate[1]-1]
-    return new_coordinate
+def move_down_left(node):
+    new_node = (node[0]-1, node[1]-1)
+    return new_node
 
-def get_moves(coordinate):
-    actions = [move_right(coordinate),move_left(coordinate),move_up(coordinate),move_down(coordinate),move_up_right(coordinate),move_up_left(coordinate),move_down_right(coordinate),move_down_left(coordinate)]
+def get_straight_moves(node):
+    actions = (move_right(node),move_left(node),move_up(node),move_down(node))
     return actions
 
-def in_rectangle(object_coordinate, obstruction, padding):
+def get_diagonal_moves(node):
+    actions = (move_up_right(node),move_up_left(node),move_down_right(node),move_down_left(node))
+    return actions
+
+def in_rectangle(node, obstruction, padding):
     return_bool = False
     x1 = obstruction[0][0] - padding
     x2= obstruction[1][0] + padding
     y1 = obstruction[0][1] - padding
     y2 = obstruction[1][1] + padding
-    xobj = object_coordinate[0]
-    yobj = object_coordinate[1]
+    xobj = node[0]
+    yobj = node[1]
 
     #print("x1:", x1," x2:", x2, " y1:", y1, " y2:", y2)
 
@@ -103,14 +108,29 @@ def in_hexagon(object_coordinate, padding):
 
     return (H1 & H2 & H3 & H4 & H5 & H6)    
 
-def in_obstacle(obstacles):
+def outside_workspace(object_coordinate, obstacle, padding):
+    return_bool = False
+    x1 = obstacle[0][0] + padding
+    x2= obstacle[1][0] - padding
+    y1 = obstacle[0][1] + padding
+    y2 = obstacle[1][1] - padding
+    xobj = object_coordinate[0]
+    yobj = object_coordinate[1]
+    if ((xobj <= x1) or (xobj >= x2) or (yobj <= y1) or (yobj >= y2)):
+        return_bool = True    
+    return return_bool
+
+def in_obstacle(object_coordinate, obstacles, padding):
     return_bool = False
     for obstacle in obstacles:
-        if len(obstacle) == 2:
-            if in_rectangle(start_coordinate, obstacle, 0):
+        if len(obstacle) == 2 and obstacle == ((0,0),(1200,500)):
+            if outside_workspace(object_coordinate, obstacle, padding):
+                    return_bool = True
+        elif len(obstacle) == 2 and obstacle != ((0,0),(1200,500)):
+            if in_rectangle(object_coordinate, obstacle, 5):
                 return_bool = True
         elif len(obstacle) == 6:
-            if in_hexagon(start_coordinate, obstacle, 5):
+            if in_hexagon(object_coordinate, 5):
                 return_bool = True
     return return_bool
 
@@ -127,8 +147,8 @@ def get_hexagon_coordinates(center_coordinate, radius, angle, padding):
     return coordinates
 
 def draw_obstacles(canvas, obstacles):
-    for obstacle in obstacles:
-        if len(obstacle) == 2:
+    for obstacle in obstacles:       
+        if len(obstacle) == 2 and obstacle != ((0,0),(1200,500)):
             start_x = obstacle[0][0]
             start_y = 500 - obstacle[0][1]  # Invert y-value
             end_x = obstacle[1][0]
@@ -140,10 +160,37 @@ def draw_obstacles(canvas, obstacles):
             polygon = np.array(obstacle)
             polygon = polygon.reshape(-1, 1, 2)
             cv2.fillPoly(canvas, [polygon], color=(0, 0, 0))
+    return
+
+def draw_explored(canvas, points, start_node, goal_node):
+    for point in points.keys():
+        # Draw the dot for the current point
+        cv2.rectangle(canvas, (point[0], 500-point[1]), (point[0] + 1, 500-point[1] + 1), color=(0, 255, 0), thickness=-1)
+
+        # Display the updated image
+        cv2.imshow('Dots', canvas)
+        cv2.waitKey(1)  # Delay between each dot drawing, adjust as needed   
+    cv2.imshow('Dots', canvas)
+    cv2.waitKey(0)
+    cv2.destroyAllWindows()     
+
+def draw_path(canvas, points):
+    for point in points:
+        # Draw the dot for the current point
+        cv2.rectangle(canvas, (point[0], 500-point[1]), (point[0] + 1, 500-point[1] + 1), color=(0, 255, 0), thickness=-1)
+
+        # Display the updated image
+        cv2.imshow('Dots', canvas)
+        cv2.waitKey(1)  # Delay between each dot drawing, adjust as needed   
+    cv2.imshow('Dots', canvas)
+    cv2.waitKey(0)
+    cv2.destroyAllWindows()  
+    return
 
 def obstacle_space():
     obstacle_list = []
 
+    obstacle_list.append(((0,0),(1200,500)))
     obstacle_list.append(((100,100),(175,500)))
     obstacle_list.append(((275,0),(350,400)))
     obstacle_list.append(((900,50),(1100,125)))
@@ -155,34 +202,104 @@ def obstacle_space():
 
     return obstacle_list
 
-def dijkstra_algorithm():
-    
-    return
+def dijkstra_algorithm(start_node, goal_node, obstacles):
+    # Create node_grid and initialize cost to come for start_node
+    node_grid = [[float('inf')] * 500 for _ in range(1200)]
+    node_grid[start_node[0]][start_node[1]] = 0
 
-# Cost to come, index, parent node index, and coordinate values (x,y)
-goal_coordinate = (1200,250)
-start_coordinate = (450,250)
-open_queue = [(0, 0, 0, start_coordinate)]
-hq.heapify(open_queue)
+    # Cost to come, coordinate values (x,y), parent
+    # Priority queue to store open nodes
+    open_queue = PriorityQueue()
+    open_queue.put((0, start_node))  # (priority, node)
 
-# Key: coordinates (x,y) , Values: Cost to come, index, parent node index 
-open_dic={}
-open_dic[start_coordinate]=(0,0,0)
+    # Dictionary to store the parent nodes
+    parent_dict = {}
 
-# Key: Parent index, Values: Cost to come, index, and coordinate values (x,y) 
-closed_dict = {}
+    # Key: nodes (x,y) , Values: parent 
+    open_dict = {}
+    open_dict  = {start_node: None}
+
+    while not open_queue.empty():
+        _, node = open_queue.get()
+        parent = open_dict.pop(node)
+        parent_dict[node] = parent
+
+        if node == goal_node:
+            print("Success")
+            return parent_dict
+
+        # Get neighboring nodes
+        straight_moves = get_straight_moves(node)
+        diagonal_moves = get_diagonal_moves(node)
+
+        for move in straight_moves:
+            if not in_obstacle(move, obstacles, 5):
+                # Calculate new cost to come
+                new_cost = node_grid[node[0]][node[1]] + 1
+                if move not in parent_dict:                
+                    # Update if new cost is lower
+                    if new_cost < node_grid[move[0]][move[1]]:
+                        if move not in open_dict:
+                            node_grid[move[0]][move[1]] = new_cost
+                            open_queue.put((new_cost, move))
+                            open_dict[move] = node
+                else:
+                    if new_cost < node_grid[move[0]][move[1]]:
+                        node_grid[move[0]][move[1]] = new_cost
+                        parent_dict[move] = node   
+
+        for move in diagonal_moves:
+            if not in_obstacle(move, obstacles, 5):
+                # Calculate new cost to come
+                new_cost = node_grid[node[0]][node[1]] + 1.4
+                if move not in parent_dict:                
+                    # Update if new cost is lower
+                    if new_cost < node_grid[move[0]][move[1]]:
+                        if move not in open_dict:
+                            node_grid[move[0]][move[1]] = new_cost
+                            open_queue.put((new_cost, move))
+                            open_dict[move] = node
+                else:
+                    if new_cost < node_grid[move[0]][move[1]]:
+                        node_grid[move[0]][move[1]] = new_cost
+                        parent_dict[move] = node  
+
+    return print("Failed to find goal")
+
+def find_path(visited_dict, start, goal):
+    current_node = goal
+    path = [current_node]
+    while start != current_node:
+        current_node = visited_dict[current_node]
+        path.insert(0, current_node)
+    return path
+
+goal_node = (1150,25)
+start_node = (15, 15)
 
 # Get obstacles
 obstacles = obstacle_space()
 
 # Initialize canvas
 canvas = np.ones((500, 1200, 3), dtype=np.uint8) * 255  # White canvas
+cv2.rectangle(canvas, (start_node[0] - 2, 500 - start_node[1] - 2), (start_node[0] + 3, 500 - start_node[1] + 3), color=(255, 0, 0), thickness=cv2.FILLED)
+cv2.rectangle(canvas, (goal_node[0] - 2, 500 - goal_node[1] - 2), (goal_node[0] + 3, 500 - goal_node[1] + 3), color=(0, 0, 255), thickness=cv2.FILLED)
 
 # Draw obstacles on canvas
 draw_obstacles(canvas, obstacles)
 
 # Display canvas
-plt.imshow(canvas)
-plt.show()
+#plt.imshow(canvas)
+#plt.show()
 
-print(in_hexagon(start_coordinate,5))
+ti = time.time()
+
+explored_dict = dijkstra_algorithm(start_node, goal_node, obstacles)
+
+path = find_path(explored_dict, start_node, goal_node)
+
+tf = time.time()
+print(tf-ti)
+
+#draw_explored(canvas, explored_dict, start_node, goal_node)
+draw_path(canvas,path)
